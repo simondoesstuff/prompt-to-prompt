a823bb4f46282de594fc8a3fce2cf4229f064dfd
Simon Walker <simon@simonwalker.tech>
2024-10-13 02:52:34 -0600

    nixified

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "cool guy ptp flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ 
+					 	# Python
+					 	python38
+					];
+        };
+      }
+		);
+}
