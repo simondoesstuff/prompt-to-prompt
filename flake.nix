1bdf7eedc85f594e14139de3512ffd77426e3251
--help <simon@simonwalker.tech>
2024-10-15 11:29:09 +0000

    nix, injecting proper libs for py

--- flake.nix
+++ flake.nix
@@ -1,20 +1,45 @@
 {
-  description = "cool guy ptp flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        python = pkgs.python38;
+
+        pyLdLibs = pkgs.lib.makeLibraryPath (with pkgs; [
+          libGL
+          stdenv.cc.cc
+          glib
+          zlib
+        ]);
+
+        # Darwin requires a different library path prefix
+        wrapPrefix = if (!pkgs.stdenv.isDarwin) then "LD_LIBRARY_PATH" else "DYLD_LIBRARY_PATH";
+        patchedPython = (pkgs.symlinkJoin {
+          name = "python";
+          paths = [ python ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram "$out/bin/python" --prefix ${wrapPrefix} : "${pyLdLibs}"
+          '';
+        });
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ 
-					 	# Python
-					 	python38
-					];
+          buildInputs = [
+            patchedPython
+          ];
         };
       }
-		);
+    );
 }
+
+
+
+
+
+
